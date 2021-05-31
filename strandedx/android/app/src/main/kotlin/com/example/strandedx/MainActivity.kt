7adb81a32da1965c59230a8c8e5fac1d5739cb5f
yeshwin <64612584+yeshwindbz9@users.noreply.github.com>
2021-05-31 21:01:36 +0530

    StrandedX
    A flutter adventure app.

---
+++ strandedx/android/app/src/main/kotlin/com/example/strandedx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.strandedx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ strandedx/lib/story.dart
@@ -0,0 +1,15 @@
+class Story{
+
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({this.storyTitle, this.choice1, this.choice2});
+  /*
+  * Story(String title, String ch1, String ch2){
+  *   storyTitle = title;
+  *   choice1 = ch1;
+  *   choice2 = ch2;
+  * }
+  * */
+}
